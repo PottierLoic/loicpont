93be595922000418217ebe76a7060ddbb2ebadd1
PottierLoic <loic.pottier@live.fr>
2022-11-28 19:14:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 
+# Auteur : Loïc Pottier
+# Date de création : 28/11/2022
+# DM Algo
+
+def menu():
+    print("Menu")
+    print("1 - Nouvelle partie")
+    print("2 - Options")
+    print("3 - Crédits")
+    print("4 - Quitter le jeu")
+
+    choix=input("Choissisez une option : ")
+    print("")
+    if choix=="4":
+        quit()
+    elif choix=="3":
+        credits()
+    elif choix=="2":
+        options()
+    elif choix=="1":
+        start()
+
+def credits():
+    print("Credits : ")
+    print("Auteur : Loïc Pottier")
+    print("Date de creation : 28/11/2022")
+    print("RATIOOOOOOOO")
+    print("")
+    menu()
+
+def options():
+    print("")
+    menu()
+
+def start():
+    tab =[]
+    for i in range(10):
+        tab.append([])
+        for j in range(10):
+            tab[i].append(0)
+    tab.append([5, 8])
+
+    
+
+
+
+
+menu()
