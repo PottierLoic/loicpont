c375e2e79e19a6e78ff574747a989ff32f7c18bc
PottierLoic <loic.pottier@live.fr>
2022-11-28 19:55:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,26 @@
 # Auteur : Loïc Pottier
 # Date de création : 28/11/2022
 
+tab=[]
+h_start=5
+h_end=2
+
+i_piece=[1, 1, 1, 1]
+o_piece=[[1, 1],
+         [1, 1]]
+t_piece=[[0, 1, 0],
+         [1, 1, 1]]
+j_piece=[[1, 1, 1],
+         [0, 0, 1]]
+l_piece=[[1, 1, 1],
+         [1, 0, 0]]
+z_piece=[[1, 1, 0],
+         [0, 1, 1]]
+s_piece=[[0, 1, 1],
+         [1, 1, 0]]
+
 def menu():
-    print("Menu")
+    print("----- LE JEU DU print RATIOOOO -----")
     print("1 - Nouvelle partie")
     print("2 - Options")
     print("3 - Crédits")
@@ -18,31 +36,46 @@ def menu():
     elif choix=="2":
         options()
     elif choix=="1":
-        start()
+        start(tab)
 
 def credits():
-    print("Credits : ")
+    print("-----Credits-----")
     print("Auteur : Loïc Pottier")
     print("Date de creation : 28/11/2022")
     print("RATIOOOOOOOO")
-    print("")
+    input("")
     menu()
 
 def options():
-    print("")
+    print("-----Options-----")
+    h_start=input("Hauteur du départ : ")
+    h_end=input("Hauteur d'arrivée : ")
+    input("") 
     menu()
 
-def start():
-    tab =[]
+def start(tab):
     for i in range(10):
         tab.append([])
         for j in range(10):
             tab[i].append(0)
-    tab.append([5, 8])
+    tab.append([h_start, h_end])
+    return tab
 
-    
+def choix():
+    print("Choix   : 0-9 : Placer la piece dans une colonne")
+    print("R : Rotation horaire de la piece")
+    print("L : Roation trigonométrique de la piece")
+    print("S : Stocker la piece")
+    ch=input("Veuillez entrer votre choix : ")
 
-
-
+def affichage():
+    for ligne in range (len(tab)-1):
+        if ligne == 5:
+            print("Départ-"+str(tab[ligne]))
+        elif ligne == 2:
+            print("       "+str(tab[ligne])+"-Arrivée ")
+        else:
+            print("       "+str(tab[ligne]))
 
 menu()
+affichage()
